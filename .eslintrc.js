f2237f55c217957f413c04ec024205eef99e75df
Idan Amati <idan.amati@gmail.com>
2020-12-19 17:13:56 +0200

    removed eslint-node

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,23 +5,13 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
-    'plugin:node/recommended',
   ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
   rules: {
-    'no-console':['off'],
-    'node/exports-style': ['error', 'module.exports'],
-    'node/file-extension-in-import': ['error', 'always'],
-    'import/extensions': ['error', 'always'],
-    'node/prefer-global/buffer': ['error', 'always'],
-    'node/prefer-global/console': ['error', 'always'],
-    'node/prefer-global/process': ['error', 'always'],
-    'node/prefer-global/url-search-params': ['error', 'always'],
-    'node/prefer-global/url': ['error', 'always'],
-    'node/prefer-promises/dns': 'error',
-    'node/prefer-promises/fs': 'error',
+    'no-console': ['off'],
+    'import/extensions': ['off'],
   },
 };
