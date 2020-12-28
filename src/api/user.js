a3812ddcadec36d00b3fb2fb2977f6ad864f475f
Idan Amati <idan.amati@gmail.com>
2020-12-28 13:27:20 +0200

    updated error msg

--- src/api/user.js
+++ src/api/user.js
@@ -54,7 +54,7 @@ const login = async (req, res) => {
     throw new Error();
   } catch (err) {
     res.status(401);
-    return res.send({ error: { msg: 'wrong credentials' } });
+    return res.send({ error: { ...err } });
   }
 };
 
